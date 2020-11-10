8bd250e00148973c1ce74bfee690dcab7c9d0339
David Edison <david.edison@hautelook.com>
2020-11-09 17:56:35 -0800

    split functions into separate files

---
+++ R/count-records.R
@@ -0,0 +1,10 @@
+
+#' Count the Records of a Table in a Remote DB
+#'
+#' @param table_name The name of the table to count the records of
+#' @param conn A DBI connection object
+#'
+#' @export
+count_records <- function(table_name, conn) {
+  DBI::dbGetQuery(conn, paste("SELECT COUNT(*) FROM", table_name))
+}
--- R/plot-dots.R
+++ R/plot-dots.R
@@ -1,14 +1,4 @@
 
-#' Count the Records of a Table in a Remote DB
-#'
-#' @param table_name The name of the table to count the records of
-#' @param conn A DBI connection object
-#'
-#' @export
-count_records <- function(table_name, conn) {
-  DBI::dbGetQuery(conn, paste("SELECT COUNT(*) FROM", table_name))
-}
-
 #' Plot Some Dots
 #'
 #' @param n_dots Number of dots to plot
