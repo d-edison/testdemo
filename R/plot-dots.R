727e3f4fad6f3283a878549cdeb31f0645f32c2a
David Edison <david.edison@hautelook.com>
2020-11-09 22:54:10 -0800

    please R CMD check

--- R/plot-dots.R
+++ R/plot-dots.R
@@ -7,8 +7,8 @@
 #' @export
 plot_dots <- function(n_dots = 100L, ...) {
   dat <- data.frame(
-    x = rnorm(n_dots),
-    y = rnorm(n_dots)
+    x = stats::rnorm(n_dots),
+    y = stats::rnorm(n_dots)
   )
   ggplot2::ggplot(dat, ggplot2::aes(x, y)) +
     ggplot2::geom_point(...)
